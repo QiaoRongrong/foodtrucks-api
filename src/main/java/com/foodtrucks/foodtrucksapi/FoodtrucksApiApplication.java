63334cc8ac568d0676115b12a019cfb21e385b64
[Jory] <larry_qr@126.com>
2023-02-07 10:29:41 +0800

    init project

---
+++ src/main/java/com/foodtrucks/foodtrucksapi/FoodtrucksApiApplication.java
@@ -0,0 +1,13 @@
+package com.foodtrucks.foodtrucksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtrucksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtrucksApiApplication.class, args);
+	}
+
+}
